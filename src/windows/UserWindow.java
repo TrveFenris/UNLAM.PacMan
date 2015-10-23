982a0a66668272cf0cf7bcee21d11e3abbd31437
TrveFenris <tinchondrius@gmail.com>
2015-10-23 18:57:59 -0300

    Create UserWindow.java
    Creado el archivo con el código base de la clase UserWindow

---
+++ src/windows/UserWindow.java
@@ -0,0 +1,169 @@
+package tallerjava.windows;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.GridBagConstraints;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class UserWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private GameWindow gameWindow;
+	private MainWindow mainWindow;
+	private ConfigWindow configWindow;
+	private JPanel contentPane;
+	private JButton btnCerrarSesion;
+	private JButton btnConfig;
+	private JButton btnJugar;
+	private JLabel lblBienvenida;
+	//CONFIGURACION
+	private String userName;
+	private String arriba;
+	private String abajo;
+	private String izquierda;
+	private String derecha;
+	
+	/* UserWindow Constructor */
+	public UserWindow(MainWindow window,String nombre) {
+		setResizable(false);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				if(mainWindow != null) {
+					confirmarCerrarSesion();
+				}
+			}
+		});
+		mainWindow = window;
+		userName=nombre;
+		setTitle("Men\u00FA principal");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 441, 263);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 97, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 13, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		lblBienvenida = new JLabel("");
+		lblBienvenida.setForeground(new Color(51, 153, 204));
+		lblBienvenida.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 18));
+		lblBienvenida.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBienvenida.setText("¡Bienvenid@ "+userName+"!");
+		GridBagConstraints gbc_lblBienvenida = new GridBagConstraints();
+		gbc_lblBienvenida.insets = new Insets(0, 0, 5, 0);
+		gbc_lblBienvenida.gridheight = 2;
+		gbc_lblBienvenida.gridwidth = 4;
+		gbc_lblBienvenida.gridx = 0;
+		gbc_lblBienvenida.gridy = 1;
+		contentPane.add(lblBienvenida, gbc_lblBienvenida);
+		
+		btnJugar = new JButton("Jugar");
+		btnJugar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lanzarJuego();
+			}
+		});
+		GridBagConstraints gbc_btnJugar = new GridBagConstraints();
+		gbc_btnJugar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnJugar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnJugar.gridx = 1;
+		gbc_btnJugar.gridy = 4;
+		contentPane.add(btnJugar, gbc_btnJugar);
+		
+		btnConfig = new JButton("Configuraci\u00F3n");
+		btnConfig.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				lanzarVentanaConfiguracion();
+			}
+		});
+		GridBagConstraints gbc_btnConfig = new GridBagConstraints();
+		gbc_btnConfig.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnConfig.insets = new Insets(0, 0, 5, 5);
+		gbc_btnConfig.gridx = 2;
+		gbc_btnConfig.gridy = 4;
+		contentPane.add(btnConfig, gbc_btnConfig);
+		
+		btnCerrarSesion = new JButton("Cerrar sesi\u00F3n");
+		btnCerrarSesion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				confirmarCerrarSesion();
+			}
+		});
+		GridBagConstraints gbc_btnCerrarSesion = new GridBagConstraints();
+		gbc_btnCerrarSesion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnCerrarSesion.gridwidth = 2;
+		gbc_btnCerrarSesion.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCerrarSesion.gridx = 1;
+		gbc_btnCerrarSesion.gridy = 6;
+		contentPane.add(btnCerrarSesion, gbc_btnCerrarSesion);
+		
+		cargarControles();
+	}
+
+	/* Métodos */
+	private void confirmarCerrarSesion(){
+		int res= JOptionPane.showConfirmDialog(this,
+			    "¿Está seguro?",
+			    "Cerrando sesion",
+			    JOptionPane.YES_NO_OPTION);
+		if(res == JOptionPane.YES_OPTION){
+			mainWindow.resetUserAndPassword();
+			dispose();
+			mainWindow.setVisible(true);
+		}
+	}
+
+	private void lanzarJuego(){
+		this.setVisible(false);
+		gameWindow = new GameWindow(this);
+		gameWindow.setNameLabel(userName);
+		gameWindow.setVisible(true);
+	}
+
+	private void cargarControles() {
+			this.arriba=new String("arriba");
+			this.abajo=new String("abajo");
+			this.derecha=new String("derecha");
+			this.izquierda=new String("izquierda");
+	}
+	
+	public void setControles(String arriba, String abajo, String derecha, String izquierda) {
+		this.arriba=arriba;
+		this.abajo=abajo;
+		this.derecha=derecha;
+		this.izquierda=izquierda;
+	}
+	
+	public String[] getControles() {
+		String[] controles=new String[4];
+		controles[0]=arriba;
+		controles[1]=abajo;
+		controles[2]=derecha;
+		controles[3]=izquierda;
+		return controles;
+	}
+	
+	private void lanzarVentanaConfiguracion() {
+		this.setVisible(false);
+		configWindow = new ConfigWindow(this);
+		configWindow.setVisible(true);
+	}
+}
