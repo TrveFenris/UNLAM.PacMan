a801f68cad78e903c80f68ceefc6562515f555a1
TrveFenris <tinchondrius@gmail.com>
2015-10-23 18:41:59 -0300

    Update GameWindow.java
    Código base de la clase GameWindow

--- src/windows/GameWindow.java
+++ src/windows/GameWindow.java
@@ -1,1 +1,70 @@
+package tallerjava.windows;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class GameWindow extends JFrame {
+	/* Members */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLabel lblName;
+	private UserWindow userWindow;
+	
+	/* Main Application */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameWindow frame = new GameWindow(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	/* Window Constructor */
+	public GameWindow(UserWindow window) {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				if(userWindow != null) {
+					mensajeSalida();
+				}
+			}
+		});
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		lblName = new JLabel("New label");
+		contentPane.add(lblName, BorderLayout.NORTH);
+		userWindow = window;
+	}
+
+	public void setNameLabel(String s) {
+		lblName.setText(s);
+	}
+
+	private void mensajeSalida() {
+		int option = JOptionPane.showConfirmDialog(this,
+			    "¿Está seguro que quiere salir?",
+			    "Saliendo del juego",
+			    JOptionPane.YES_NO_OPTION);
+		if(option == JOptionPane.YES_OPTION){
+			this.dispose();
+			userWindow.setVisible(true);
+		}
+	}
+}
