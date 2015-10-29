2b3e82f45a7523db923e10633fd26da362219ddf
TrveFenris <tinchondrius@gmail.com>
2015-10-29 18:17:55 -0300

    Changed Mapa.java
    - Cambiado el array primitivo por un ArrayList

--- src/game/Mapa.java
+++ src/game/Mapa.java
@@ -1,17 +1,17 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Mapa {
-	private Recta [] rectas;
 	
-	public Mapa(){
-		rectas = new Recta [10]; /*Cambiar el 10 por la dimensión que corresponda*/
-		/* Creo las distintas rectas (hardcodeadas) que conforman el mapa */
-		/* crea una recta vertical, p ej */
-		//agregarRecta(new Punto(10,10), new Punto(10,50), 0); 
+	private ArrayList<Recta> rectas;
+	
+	public Mapa() {
+		rectas = new ArrayList<Recta>();
+		agregarRecta(new Punto(10,10), new Punto(10,50));
 	}
 	
-	private void agregarRecta(Punto p1, Punto p2, int i) {
-		if(i < rectas.length)
-			rectas[i] = new Recta(p1,p2);
+	private void agregarRecta(Punto p1, Punto p2) {
+		rectas.add(new Recta(p1,p2));
 	}
 }
