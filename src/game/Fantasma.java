5643fc0dbd8dfdca738f537680f7fe3e9c2872de
matiasmcf <matiasmcf@gmail.com>
2015-11-02 21:14:08 -0300

    Fantasma - Cambios varios
    -Agregados atributos para el manejo de puntajes: muertes, pacmansComidos, veces aturdidos.
    -El método "mover" ahora forma parte de la clase "Jugador".
    -Agregados métodos: dibujar, actualizarPuntaje, y crearLabel.
    -El método crearLabel crea el JLabel con la imagen, tamaño y posicion. (No se realiza en el constructor porque la llamada a "super" debe ser sí o sí la primera línea del constructor, por lo que no permite armar el label antes de enciarlo.)

--- src/game/Fantasma.java
+++ src/game/Fantasma.java
@@ -1,19 +1,35 @@
 package game;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Fantasma extends Jugador{
+	protected int pacmansComidos;
+	protected int vecesAturdido;
+	protected int muertes;
 
-	//public Fantasma(int x, int y, JLabel img) {
-	public Fantasma(JLabel img) {
-		super(img);
+	public Fantasma(JLabel img,String nombre) {
+		super(img,nombre,1);
+		pacmansComidos=vecesAturdido=muertes=0;
 	}
 
-	public void mover(){
+	public void reaparecer(){
 		
 	}
 	
-	public void reaparecer(){
-		
+	public void dibujar(JPanel area){
+		area.add(getImagen());
+	}
+	
+	public void actualizarPuntaje(){
+		this.actualizarPuntaje(500*pacmansComidos-100*vecesAturdido-200*muertes);
+	}
+	
+	public static JLabel crearLabel(Punto posInicial){
+		ImageIcon icon = new ImageIcon("img/pacman.gif");
+		JLabel l = new JLabel(icon);
+		l.setBounds(posInicial.getX(),posInicial.getY(), 50, 50);
+		return l;
 	}
 }
