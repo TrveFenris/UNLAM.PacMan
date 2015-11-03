de1ce16aef31bdc265366b9503e5143015c1eab0
matiasmcf <matiasmcf@gmail.com>
2015-11-02 21:07:58 -0300

    Bolita - Eliminados comentarios

--- src/game/Bolita.java
+++ src/game/Bolita.java
@@ -5,14 +5,9 @@ import javax.swing.JLabel;
 public class Bolita extends GameObject{
 	
 	private boolean superpoder;
-	
-	//public Bolita(int x, int y, JLabel img, boolean especial) {
 	public Bolita(JLabel img, boolean especial) {
 		super(img);
 		superpoder = especial;
-		/*if(superpoder == true) {
-			radio = 2;
-		}*/
 	}
 	
 	public boolean isEspecial(){
