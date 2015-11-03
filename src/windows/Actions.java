f56b0081d351bbb75a9d8d67c114293ad3ab1d0b
matiasmcf <matiasmcf@gmail.com>
2015-11-02 21:36:10 -0300

    Actions - Ahora se usan
     -Eliminadas cosas raras (tengo un backup de todo)

--- src/windows/Actions.java
+++ src/windows/Actions.java
@@ -1,14 +1,8 @@
 package windows;
 
 public enum Actions {
-	ARRIBA(0),
-	ABAJO(1),
-	IZQUIERDA(2),
-	DERECHA(3);
-	
-	private final int actionCode;
-
-    private Actions(int actionCode) {
-        this.actionCode = actionCode;
-    }
+	ARRIBA,
+	ABAJO,
+	IZQUIERDA,
+	DERECHA;
 }
