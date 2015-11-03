8dc320f2ecb93962e58e8ca7b9bb09aa861a4ae5
matiasmcf <matiasmcf@gmail.com>
2015-11-02 21:34:17 -0300

    Rectas - Cambios varios
    -Se utiliza el método "modulo" para establecer como punto de destino el más cercano al origen.
    -Agregado el método "dibujar".
    -Hay que buscar una forma de validar si con los puntos dados se puede o no armar una recta NO oblicua, e informarlo o hacer algo al respecto (o no).

--- src/game/Recta.java
+++ src/game/Recta.java
@@ -1,19 +1,28 @@
 package game;
 
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Recta {
 	protected Punto puntoInicial;
 	protected Punto puntoFinal;
 	protected Rectas tipo;
 	
 	protected Recta(Punto p1, Punto p2) {
-		puntoInicial = p1;
-		puntoFinal = p2;
-		if(puntoInicial.getX() == puntoFinal.getX()) {
+		if(p1.modulo()>p2.modulo()){
+			puntoInicial = p2;
+			puntoFinal = p1;
+		}
+		else{
+			puntoInicial = p1;
+			puntoFinal = p2;
+		}
+		if(puntoInicial.getX() == puntoFinal.getX())
 			tipo = Rectas.VERTICAL;
-		}
-		else if(puntoInicial.getY() == puntoFinal.getY()) {
-			tipo = Rectas.HORIZONTAL;
-		}
+		else 
+			if(puntoInicial.getY() == puntoFinal.getY())
+				tipo = Rectas.HORIZONTAL;
 	}
 	
 	public Rectas getTipo(){
@@ -26,5 +35,26 @@ public class Recta {
 	
 	public Punto getPuntoFinal() {
 		return puntoFinal;
+	}
+	
+	public int getLongitud(){
+		int l=0;
+		if(tipo==Rectas.HORIZONTAL)
+			l= puntoFinal.getX()-puntoInicial.getX();
+		else if(tipo==Rectas.VERTICAL)
+			l= puntoFinal.getY()-puntoInicial.getY();
+		return l;
+	}
+	
+	public void dibujar(JPanel area){
+		JLabel camino = new JLabel();
+		if(tipo==Rectas.HORIZONTAL)
+			camino.setBounds(puntoInicial.getX(), puntoInicial.getY(), puntoFinal.getX()-puntoInicial.getX(), 1);
+		else
+			if(tipo==Rectas.VERTICAL)
+				camino.setBounds(puntoInicial.getX(), puntoInicial.getY(), 1, puntoFinal.getY()-puntoInicial.getY());
+		camino.setBackground(Color.GREEN);
+		camino.setOpaque(true);
+		area.add(camino);
 	}
 }
