a4ee907a8cff9c62c66a930e74c0b7f89b3b632f
TrveFenris <tinchondrius@gmail.com>
2015-11-07 11:10:19 -0300

    Update ThreadServerSesion.java
    - Habilitado el acknowledgment del acceso en la base de datos

--- src/server/ThreadServerSesion.java
+++ src/server/ThreadServerSesion.java
@@ -35,16 +35,16 @@ public class ThreadServerSesion extends Thread {
     	            //ACCIONES A REALIZAR SEGUN EL TIPO DE PAQUETE RECIBIDO
     	            switch(paquete.getTipoPaquete()){
     	            	case LOGIN:
-    	            		paquete.setAck(true);
-    	            		//paquete.setAck(database.verificarDatos(paquete.getNombre(), paquete.getPassword()));
+    	            		//paquete.setAck(true);
+    	            		paquete.setAck(database.verificarDatos(paquete.getNombre(), paquete.getPassword()));
     	            		break;
     	            	case LOGOUT:
     	            		paquete.setAck(true);
     	            		run=false;
     	            		break;
     	            	case REGISTRO:
-    	            		paquete.setAck(false);
-    	            		//paquete.setAck(database.registrarUsuario(paquete.getNombre(), paquete.getPassword()));
+    	            		//paquete.setAck(false);
+    	            		paquete.setAck(database.registrarUsuario(paquete.getNombre(), paquete.getPassword()));
     	            		break;
     	            }
     	            o.writeObject(paquete);
