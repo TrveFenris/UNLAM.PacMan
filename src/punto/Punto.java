ec20e2393d971d1fcfd6c9d99aec93047e4ec8ad
matiasmcf <matiasmcf@gmail.com>
2015-11-11 18:03:02 -0300

    Serializable

--- src/punto/Punto.java
+++ src/punto/Punto.java
@@ -1,6 +1,9 @@
 package punto;
 
-public class Punto {
+import java.io.Serializable;
+
+public class Punto implements Serializable{
+	private static final long serialVersionUID = 18L;
 	private int x;
 	private int y;
 	
