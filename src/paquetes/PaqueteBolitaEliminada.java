7a3ac24c2e86d161d91b0ca74bd7aa4c42eb9869
TrveFenris <tinchondrius@gmail.com>
2015-11-14 20:45:06 -0300

    Descripción de PaqueteBolitaEliminada
    - Agregados Doc Comments.

--- src/paquetes/PaqueteBolitaEliminada.java
+++ src/paquetes/PaqueteBolitaEliminada.java
@@ -1,5 +1,12 @@
 package paquetes;
 
+/**
+ * 
+ * Esta clase es un paquete que sirve para comunicar al servidor qué bolita del ArrayList de bolitas 
+ * contenido en una partida fue eliminada por algún cliente, y también permite al servidor propagar
+ * entre sus clientes dicha acción.
+ * @author Fenris
+ */
 public class PaqueteBolitaEliminada {
 	private int index;
 	public PaqueteBolitaEliminada(int i) {
