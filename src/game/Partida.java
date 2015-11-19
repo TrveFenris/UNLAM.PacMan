b10fde11c263a94b0ccd36c65c3499234bf50dff
TrveFenris <tinchondrius@gmail.com>
2015-11-19 17:23:14 -0300

    Cambios en Partida
    - Agregado un Set de ID's (inicializado en el constructor) que cederá o
    recuperará ID's de los usuarios.

--- src/game/Partida.java
+++ src/game/Partida.java
@@ -3,17 +3,35 @@ package game;
 import gameobject.Jugador;
 
 import java.util.ArrayList;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Partida {
 	private Mapa mapa;
 	private ArrayList<Jugador> jugadores;
 	private boolean activa;
 	private String nombre;
+	private SortedSet<Integer> idsDisponibles;
 	
 	public Partida(String name) {
 		jugadores = new ArrayList<Jugador>();
 		activa = true;
 		nombre = name;
+		idsDisponibles = new TreeSet<Integer>();
+		idsDisponibles.add(1);
+		idsDisponibles.add(2);
+		//idsDisponibles.add(3);
+		//idsDisponibles.add(4);
+	}
+	//No se si se va a usar, pero ante la duda...
+	public SortedSet<Integer> getIdsDisponibles() {
+		return idsDisponibles;
+	}
+	public void tomarID(Integer id) {
+		idsDisponibles.remove(id);
+	}
+	public void liberarID(Integer id) {
+		idsDisponibles.add(id);
 	}
 	
 	public void setActiva(boolean state) {
