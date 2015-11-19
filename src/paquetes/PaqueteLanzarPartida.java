7f701054aa7a432df447ab1f0d5257ca8b7fca8e
TrveFenris <tinchondrius@gmail.com>
2015-11-19 18:37:45 -0300

    Cambios en PaqueteLanzarPartida
    - Estaba mal planteado, se construye sin parámetros y el servidor setea
    el true/false (si es posible lanzar la partida o no).

--- src/paquetes/PaqueteLanzarPartida.java
+++ src/paquetes/PaqueteLanzarPartida.java
@@ -3,8 +3,11 @@ package paquetes;
 public class PaqueteLanzarPartida extends Paquete {
 	private static final long serialVersionUID = 9176381048245973322L;
 	private boolean ready;
-	public PaqueteLanzarPartida(boolean status) {
-		ready = status;
+	public PaqueteLanzarPartida() {
+		ready = false;
+	}
+	public void setReady(boolean estado) {
+		ready = estado;
 	}
 	public boolean isReady() {
 		return ready;
