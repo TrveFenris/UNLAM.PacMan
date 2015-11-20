7b7b12eea0debe6f6a83e31d5b1b486f4d6819fe
TrveFenris <tinchondrius@gmail.com>
2015-11-20 16:01:48 -0300

    Fix en Server
    - El server ahora al crear una partida genera las bolitas de la misma.

--- src/server/Server.java
+++ src/server/Server.java
@@ -266,6 +266,7 @@ public class Server {
 		else {
 			Partida p = new Partida(nombre);
 			p.agregarMapa(new Mapa("mapaoriginal"));//TODO Ojo con el mapa, con cada uno cambia el punto de "spawn"
+			p.getMapa().generarBolitas();
 			partidas.put(p,new ArrayList<Usuario>());
 			nombresDePartida.put(nombre, p);
 			return true;
