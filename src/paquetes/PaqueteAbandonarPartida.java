6910669fadcd5da2ac85bafab7d2de6ba0beb055
matiasmcf <matiasmcf@gmail.com>
2015-11-20 13:12:52 -0300

    Creado paquete para abandono de partida

--- src/paquetes/PaqueteAbandonarPartida.java
+++ src/paquetes/PaqueteAbandonarPartida.java
@@ -1,14 +1,25 @@
 package paquetes;
 
-public class PaqueteAbandonarPartida extends Paquete{
+public class PaqueteAbandonarPartida extends Paquete {
 
-	private static final long serialVersionUID = 2236084928910201115L;
-	
+	private static final long serialVersionUID = 6799481996971774797L;
+	private boolean pacman;
+
 	public PaqueteAbandonarPartida() {
+		pacman=false;
 	}
 	
 	@Override
 	public TipoPaquete getTipo() {
 		return TipoPaquete.ABANDONAR_PARTIDA;
 	}
+	
+	public void setPacman(){
+		pacman=true;
+	}
+	
+	public boolean isPacman(){
+		return pacman;
+	}
+
 }
