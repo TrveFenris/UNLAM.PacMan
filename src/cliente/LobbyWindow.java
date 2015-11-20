0536e85e26df0543e427af2ecab8e78bf1d19977
TrveFenris <tinchondrius@gmail.com>
2015-11-20 15:12:14 -0300

    Cambios en LobbyWindow
    - Aggiornada a la estructura del juego actual. Eliminados botones y
    métodos innecesarios.

--- src/cliente/LobbyWindow.java
+++ src/cliente/LobbyWindow.java
@@ -2,7 +2,6 @@ package cliente;
 
 import game.ConfiguracionSprites;
 
-import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -10,12 +9,10 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
 import paquetes.PaqueteAbandonarLobby;
@@ -27,15 +24,12 @@ public class LobbyWindow extends JFrame {
 	private JPanel contentPane;
 	private UserWindow mainWindow;
 	private LobbyWindow thisWindow;
-	private JComboBox<String> comboBoxPartidas;
 	private JLabel lblReady;
 	private boolean ready;
-	private int IDJugadorLocal;
 	private ListenThread thread;
 	private ImageIcon iconoReady;
 	private ImageIcon iconoNotReady;
 
-	//public LobbyWindow(ArrayList<AbstractMap.SimpleImmutableEntry<String, Integer>> datosPartidas, UserWindow main) {
 	public LobbyWindow(String nombrePartida, UserWindow main) {
 		setResizable(false);
 		
@@ -43,14 +37,12 @@ public class LobbyWindow extends JFrame {
 			@Override
 			public void windowClosing(WindowEvent arg0) {
 				if(mainWindow != null) {
-					//Si cierro la ventana deberia quitarme de la partida
 					confirmarSalirDelLobby();
 				}
 			}
 		});
 		mainWindow = main;
 		thisWindow = this;
-		//setTitle("Seleccion de partida");
 		setTitle("Lobby de la partida \""+nombrePartida+"\"");
 		iconoReady = new ImageIcon(ConfiguracionSprites.ICONO_READY.getValor());
 		iconoNotReady = new ImageIcon(ConfiguracionSprites.ICONO_NOTREADY.getValor());
@@ -61,51 +53,14 @@ public class LobbyWindow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblSeleccioneUnaPartida = new JLabel("Seleccione una partida:");
-		lblSeleccioneUnaPartida.setBounds(35, 35, 359, 14);
-		contentPane.add(lblSeleccioneUnaPartida);
-		
-		comboBoxPartidas = new JComboBox<String>();
-		comboBoxPartidas.setBounds(35, 54, 359, 20);
-		contentPane.add(comboBoxPartidas);
-		
-		JLabel lblNombrePartida = new JLabel("Partida");
-		lblNombrePartida.setBounds(106, 79, 34, 14);
-		contentPane.add(lblNombrePartida);
-		
-		JLabel lblCantidadDeJugadores = new JLabel("Cantidad de jugadores");
-		lblCantidadDeJugadores.setBounds(251, 79, 109, 14);
-		contentPane.add(lblCantidadDeJugadores);
-		
-		JTextArea textAreaNombres = new JTextArea();
-		textAreaNombres.setBounds(35, 98, 177, 65);
-		textAreaNombres.setBackground(SystemColor.control);
-		contentPane.add(textAreaNombres);
-		
-		JTextArea textAreaCantJugadores = new JTextArea();
-		textAreaCantJugadores.setBounds(217, 98, 177, 65);
-		textAreaCantJugadores.setBackground(SystemColor.control);
-		contentPane.add(textAreaCantJugadores);
-		
-		JButton btnUnirse = new JButton("Unirse");
-		btnUnirse.setBounds(35, 198, 177, 23);
-		btnUnirse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//lanzarJuego();
-			}
-		});
-		contentPane.add(btnUnirse);
-		
 		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(217, 198, 177, 23);
+		btnCancelar.setBounds(257, 237, 177, 23);
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				confirmarSalirDelLobby();
 			}
 		});
 		contentPane.add(btnCancelar);
-		
-		textAreaNombres.setText("");
 		
 		JButton btnJugadorListo = new JButton("Listo");
 		btnJugadorListo.addActionListener(new ActionListener() {
@@ -114,18 +69,16 @@ public class LobbyWindow extends JFrame {
 					mainWindow.getCliente().enviarDatosPartida(new PaquetejugadorListo(true));
 					lblReady.setIcon(iconoReady);
 					ready = true;
-					//Cambiar el label ready a una tilde
 				}
 				else {
 					mainWindow.getCliente().enviarDatosPartida(new PaquetejugadorListo(false));
 					lblReady.setIcon(iconoNotReady);
 					ready = false;
-					//cambiar el label ready a una cruz
 				}
 				System.out.println(ready);
 			}
 		});
-		btnJugadorListo.setBounds(35, 237, 89, 23);
+		btnJugadorListo.setBounds(10, 204, 89, 23);
 		contentPane.add(btnJugadorListo);
 		
 		JButton btnLanzarPartida = new JButton("Lanzar Partida");
@@ -134,38 +87,16 @@ public class LobbyWindow extends JFrame {
 				mainWindow.getCliente().enviarDatosPartida(new PaqueteLanzarPartida());
 			}
 		});
-		btnLanzarPartida.setBounds(227, 237, 109, 23);
+		btnLanzarPartida.setBounds(148, 204, 109, 23);
 		contentPane.add(btnLanzarPartida);
 		
 		lblReady = new JLabel();
-		lblReady.setBounds(134, 232, 34, 28);
+		lblReady.setBounds(108, 199, 34, 28);
 		lblReady.setIcon(iconoNotReady);
 		contentPane.add(lblReady);
 		thread = new ListenThread();
 		thread.start();
-		/*
-		for(SimpleImmutableEntry<String, Integer> s : datosPartidas){
-			textAreaNombres.setText(textAreaNombres.getText()+s.getKey()+"\n");
-			textAreaCantJugadores.setText(textAreaCantJugadores.getText()+s.getValue()+"\n");
-			comboBoxPartidas.addItem(s.getKey());
-		}
-		*/
 	}
-/*
-	private void lanzarJuego() {
-		if(mainWindow.getCliente().unirseAPartida(comboBoxPartidas.getSelectedItem().toString()) ) {
-			System.out.println("Entrando a la partida");
-			//Recibir paquete partida.
-		}
-		else {
-			System.out.println("Error al unirse a la partida");
-			return;
-		}
-		this.setVisible(false);
-		//mainWindow.lanzarJuego();
-		dispose();
-	}
-	*/
 	
 	private void confirmarSalirDelLobby(){
 		int res= JOptionPane.showConfirmDialog(this,
@@ -174,12 +105,12 @@ public class LobbyWindow extends JFrame {
 			    JOptionPane.YES_NO_OPTION);
 		if(res == JOptionPane.YES_OPTION) {
 			thread.pararThread();
+			//Aviso al servidor para que me elimine de la partida
 			mainWindow.getCliente().enviarDatosPartida(new PaqueteAbandonarLobby());
 			setVisible(false);
 			mainWindow.setVisible(true);
 			dispose();
 			System.out.println("Saliendo de la partida");
-			//Aviso al servidor para que me elimine de la partida
 		}
 	}
 	
