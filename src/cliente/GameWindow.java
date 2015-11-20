b505a57d29cd991e82af3a043778ba10ebc28d27
matiasmcf <matiasmcf@gmail.com>
2015-11-20 20:44:41 -0300

    GameWindow - Comentado el calculo de colisiones

--- src/cliente/GameWindow.java
+++ src/cliente/GameWindow.java
@@ -228,7 +228,7 @@ public class GameWindow extends JFrame {
 		jugadorLocal.mover();
 		restrictBoundaries(jugadorLocal);
 		userWindow.getCliente().enviarPosicion(jugadorLocal); //Aun no anda porque no recibo un ID generado por el server
-		calcularColisiones (jugadorLocal);
+		//calcularColisiones (jugadorLocal);
 	}
 	
 	/**
